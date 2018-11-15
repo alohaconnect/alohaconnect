1590f54c56b03a80dac94c5b93723094e1692c8b
Trevin Tejero <trevin808@gmail.com>
2018-11-14 19:52:14 -1000

    Issue 1: Finished mockup landing page

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = {backgroundColor: '#1B1C1D', color: 'white'};
     return (
-        <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
+        <footer style={divStyle}>
+          <div className="ui center aligned container">
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
               Honolulu, HI 96822
