2e4e0eac53386f2f601e02a8d3d0945a14787188
dawei-yang <33886534+dawei-yang@users.noreply.github.com>
2018-11-23 17:16:06 -1000

    working

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import 'semantic-ui-css/semantic.css';
@@ -7,9 +7,6 @@ import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
 import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
--- app/imports/ui/pages/AddPositionCard.jsx
+++ app/imports/ui/pages/AddPositionCard.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { Positions, PositionSchema } from '/imports/api/position/position';
-import { Grid, Segment, Header, Field } from 'semantic-ui-react';
+import { Grid, Segment, Header, Dropdown, Select,Label, Form , Field} from 'semantic-ui-react';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
 import SubmitField from 'uniforms-semantic/SubmitField';
 import HiddenField from 'uniforms-semantic/HiddenField';
 import ErrorsField from 'uniforms-semantic/ErrorsField';
+import SelectField from 'uniforms-semantic/SelectField';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Meteor } from 'meteor/meteor';
 
+let input = [];
+const options = [
+  { key: 'java', text: 'java', value: 'java' },
+  { key: 'python', text: 'python', value: 'python' },
+];
 
 /** Renders the Page for adding a document. */
-class AddPosition extends React.Component {
+class AddPositionCard extends React.Component {
 
   /** Bind 'this' so that a ref to the Form can be saved in formRef and communicated between render() and submit(). */
   constructor(props) {
     super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.submit = this.submit.bind(this);
+    this.change = this.change.bind(this);
+    this.setState = this.setState.bind(this);
     this.insertCallback = this.insertCallback.bind(this);
     this.formRef = null;
   }
@@ -31,6 +42,14 @@ class AddPosition extends React.Component {
     }
   }
 
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+  }
+
   /** On submit, insert the data. */
   submit(data) {
     const { name, requirement, description } = data;
@@ -38,17 +57,25 @@ class AddPosition extends React.Component {
     Positions.insert({ name, requirement, description, owner }, this.insertCallback);
   }
 
+
+  change(data) {
+    console.log(data.target);
+  }
+
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
     return (
         <Grid container centered>
           <Grid.Column>
-            <Header as="h2" textAlign="center">Add Position</Header>
-            <AutoForm ref={(ref) => { this.formRef = ref; }} schema={PositionSchema} onSubmit={this.submit}>
+            <Header as="h2" textAlign="center">Add Position Card ver</Header>
+            <Dropdown placeholder='Skills' fluid multiple selection options={options} onChange = {this.change} >
+            </Dropdown>
+            <AutoForm ref={(ref) => { this.formRef = ref; }} schema={PositionSchema} onSubmit={this.submit} >
               <Segment>
                 <TextField name='name'/>
                 <TextField name='description'/>
-                <TextField name='requirement'/>
+                <ListAdd name='requirement' options={options} disable='enable'>
+                </ListAdd>
                 <SubmitField value='Submit'/>
                 <ErrorsField/>
                 <HiddenField name='owner' value='fakeuser@foo.com'/>
@@ -60,4 +87,4 @@ class AddPosition extends React.Component {
   }
 }
 
-export default AddPosition;
+export default AddPositionCard;
